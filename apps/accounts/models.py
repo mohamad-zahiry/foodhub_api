83addbbdaa24cffcf9deb02ca0060d7f20fa473e
Mohamad Zahiri <mamadzhri@gmail.com>
2023-08-21 20:43:50 +0330

    use email for registering user
    files:
        apps/accounts/modesl.py:
            UserManager: create user with email and password
            User.username: remove username

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,17 +1,49 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 from phonenumber_field.modelfields import PhoneNumberField
 
 
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a user with the given email, and password"""
+        if not email:
+            raise ValueError("The given email must be set")
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     name = models.CharField(max_length=128)
     phone = PhoneNumberField()
-    email = models.EmailField(unique=True)
     coupons = models.ManyToManyField(to="loyalty_club.Coupon")
+    username = None
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
 
 class Address(models.Model):
