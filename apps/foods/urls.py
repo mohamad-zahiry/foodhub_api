caff3b23805a3e44496a5a05c89bfa517d8d01af
Mohamad Zahiri <mamadzhri@gmail.com>
2023-09-01 17:25:00 +0330

    add get food by category api view
    files:
        apps/foods/views.py:
            FoodByCategoryView: can paginate foods by given category
    
        apps/foods/urls.py:
            add /api/foods/category/<given category>/ api route
    
        apps/foods/pagination.py:
            create pagination for FoodByCategoryView

--- apps/foods/urls.py
+++ apps/foods/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ListCreateFoodsView, CategoriesView
+from .views import ListCreateFoodsView, CategoriesView, FoodByCategoryView
 
 basename = "foods"
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path("categories/", CategoriesView.as_view(), name="categories"),
     path("", ListCreateFoodsView.as_view(http_method_names=["get"]), name="list_foods"),
     path("create/", ListCreateFoodsView.as_view(http_method_names=["post"]), name="create_food"),
+    path("category/<str:category>/", FoodByCategoryView.as_view(), name="food_by_category"),
 ]
--- apps/foods/views.py
+++ apps/foods/views.py
@@ -3,12 +3,28 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import Food
+from .pagination import FoodByCategoryPagination
 from .serializers import FoodSerializer
 
 
 class CategoriesView(APIView):
     def get(self, request, *args, **kwargs):
         return Response(Food.Category.choices)
+
+
+class FoodByCategoryView(generics.ListAPIView):
+    serializer_class = FoodSerializer
+    pagination_class = FoodByCategoryPagination
+
+    def get_queryset(self):
+        category = self.kwargs["category"].title()
+        try:
+            Food.Category.labels.index(category)
+        except ValueError:
+            # if the category doesnt' exist, return empty queryset
+            return Food.objects.none()
+
+        return Food.objects.filter(category=category[0])
 
 
 class ListCreateFoodsView(generics.ListCreateAPIView):
